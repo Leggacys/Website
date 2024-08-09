c73076484fe3049f080c230439e02539010a8225
Daniel Bogatu <daniel.bogatu@amplified.software>
2024-08-09 20:42:30 +0200

    Add commands

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,32 @@
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+
+        public class Comand:IRequest{
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Comand>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task Handle(Comand request, CancellationToken cancellationToken)
+            {
+                _context.Activities.Add(request.Activity);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+    }
+}
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,28 +4,28 @@ import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
 function App() {
-const [activities, setActivities] = useState([])
+  const [activities, setActivities] = useState([])
 
-useEffect(() => {
-  axios.get('http://localhost:5000/api/activities')
-    .then(response =>{
-      console.log(response)
-      setActivities(response.data)
-    })
-},[]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        console.log(response)
+        setActivities(response.data)
+      })
+  }, []);
 
-return (
-  <div>
-    <Header as='h2' icon='users' content='Reactivities' />
-    <List>
-      {activities.map((activity:any) => (
-        <li key={activity.id}>
-          {activity.title}
-        </li>
-      ))}
-    </List> 
-  </div>
-   
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+        {activities.map((activity: any) => (
+          <li key={activity.id}>
+            {activity.title}
+          </li>
+        ))}
+      </List>
+    </div>
+
   )
 }
 
